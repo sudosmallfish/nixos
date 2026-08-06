f0f6606c332d5f1e0dd880afc7cbf07df6a025af
sudosmallfish <rgf8224@gmail.com>
2026-08-06 00:56:09 +0000

    Add files via upload

---
+++ myFlake/modules/hosts/my-machine/hardware.nix
@@ -0,0 +1,23 @@
+{ self, inputs, ... }: {
+
+	flake.nixosModules.myMachineHardware = { config, lib, pkgs, modulesPath, ... }: {
+  		imports = [ 
+			(modulesPath + "/installer/scan/not-detected.nix")
+		];
+
+  		boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "sd_mod" "sr_mod" ];
+  		boot.initrd.kernelModules = [ ];
+  		boot.kernelModules = [ ];
+  		boot.extraModulePackages = [ ];
+
+  		fileSystems."/" = {
+    			device = "/dev/disk/by-uuid/c1010119-3018-4738-9dda-8133c21fbc8a";
+      			fsType = "ext4";
+   		};
+
+  		swapDevices = [ ];
+
+		nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  		virtualisation.virtualbox.guest.enable = true;
+	};
+}
